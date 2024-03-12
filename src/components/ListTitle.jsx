df97d28ad62775e590275de0f819a1fa4165f281
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-03-12 08:59:18 +0100

    makeStyles and icons from materal-ui

--- src/components/ListTitle.jsx
+++ src/components/ListTitle.jsx
@@ -1,8 +1,32 @@
+import { Typography, makeStyles } from "@material-ui/core";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 export const ListTitle = () => {
-  return (
-    <div>
-        <h1>To do</h1>
+  
+    const classes = useStyle();
+    return (
+    
+    <div className={classes.title}>
+        <Typography className={classes.titleText}>
+            To do
+        </Typography>
+        <MoreHorizIcon />
     </div>
   )
 }
+
+const useStyle = makeStyles(theme => ({
+    title: {
+        display: 'flex',
+        alignItems: 'center',
+        margin: theme.spacing(1),
+    },
+    titleText: {
+        flexGrow: 1,
+        fontWeight: 'bold',
+        fontSize: '1.2rem',
+        fontWeight: 'bold',
+
+    }
+  
+  }))
--- src/components/TrelloList.jsx
+++ src/components/TrelloList.jsx
@@ -1,16 +1,28 @@
-import { Paper, CssBaseline } from '@material-ui/core';
+import { Paper, CssBaseline, makeStyles } from '@material-ui/core';
 import { ListTitle } from './ListTitle';
 import { TrelloCard } from './TrelloCard';
 import { AddCardorList } from './AddCardorList';
+
+const useStyle = makeStyles(theme => ({
+  root: {
+    width: '300px',
+    backgroundColor: '#ebecf0',
+    margin: theme.spacing(1)
+  }
+
+}))
+
 export const TrelloList = () => {
+  
+  const classes = useStyle();
   return (
-    <Paper>
+    
+    <Paper className={classes.root}>
       <CssBaseline />
       <ListTitle />
       <TrelloCard />
       <TrelloCard />
       <AddCardorList />
-
     </Paper>
   )
 }
