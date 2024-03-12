3402858a1b119b9dee0813c2482427ba58544bc1
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-03-12 16:17:28 +0100

    AddCardorListext ready

--- src/components/AddCardorListText.jsx
+++ src/components/AddCardorListText.jsx
@@ -1,8 +1,65 @@
+import { Button, alpha, IconButton, InputBase, makeStyles, Paper } from '@material-ui/core';
+import { useState } from 'react';
+import ClearIcon from '@material-ui/icons/Clear';
+import { MoreHoriz } from '@material-ui/icons';
 
 export const AddCardorListText = () => {
-  return (
-    <div>
-        <h2>Write a title...</h2>
-    </div>
+    const [title, setTitle] = useState('');
+    const classes = useStyle();
+    
+    return (
+    <>
+        <Paper className={classes.card}>
+            <InputBase 
+                multiline
+                value={title} 
+                onChange={e=>setTitle(e.target.value)}
+                placeholder='Enter a title...'
+                inputProps={{className: classes.input}}
+            />
+        </Paper>
+        <div className={classes.confirm}>
+            <div className={classes.options}>   
+                <Button className={classes.btnConfirm}>Add card</Button>
+                <IconButton>
+                    <ClearIcon />
+                </IconButton>
+            </div>
+                <IconButton>
+                    <MoreHoriz />
+                </IconButton> 
+            </div>
+
+    </>
   )
 }
+
+const useStyle = makeStyles( theme => ({
+    card: {
+        width: '280px',
+        margin: theme.spacing(0, 1, 1, 1),
+        paddingBottom: theme.spacing(4),
+    },
+    input: {
+        margin: theme.spacing(1),
+    },
+    confirm: {
+        display: "flex",
+        margin: theme.spacing(0,1,1,0),
+    }, 
+    options: {
+        flexGrow: 1,
+        display: "flex", 
+        
+    },
+    btnConfirm: {
+        background: '#5aac44',
+        color: "#fff",
+        "&:hover": {
+            background: alpha("#5aac44", 0.75)
+        },
+        margin: theme.spacing(1),
+        
+    },
+  
+  }))
